b3c77adab54d277dcc586b81d9587a6efc2758da
Nobime Romeo <nobimeromeo@pop-os.localdomain>
2023-02-06 16:08:29 +0100

    lettings templates modification

--- lettings/views.py
+++ lettings/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .models import Letting
 
 
-def lettings_index(request):
+def index(request):
     """Aenean leo magna, vestibulum et tincidunt fermentum,
     consectetur quis velit. Sed non placerat massa.
     Integer est nunc, pulvinar a tempor et, bibendum id arcu.
@@ -10,7 +10,7 @@ def lettings_index(request):
     ultrices posuere cubilia curae; Cras eget scelerisque"""
     lettings_list = Letting.objects.all()
     context = {"lettings_list": lettings_list}
-    return render(request, "lettings_index.html", context)
+    return render(request, "lettings/index.html", context)
 
 
 def letting(request, letting_id):
