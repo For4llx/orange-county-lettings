966d4e1a6f78f8e1c1f60c6a16f06d35b5a3cb86
Nobime Romeo <nobimeromeo@pop-os.localdomain>
2023-02-06 15:52:01 +0100

    fix bug url

--- profiles/urls.py
+++ profiles/urls.py
@@ -4,6 +4,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("profiles/", views.profiles_index, name="profiles_index"),
-    path("profiles/<str:username>/", views.profile, name="profile"),
+    path("", views.profiles_index, name="profiles_index"),
+    path("<str:username>/", views.profile, name="profile"),
 ]
